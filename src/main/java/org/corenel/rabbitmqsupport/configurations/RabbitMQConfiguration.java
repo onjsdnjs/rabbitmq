135cab404f9a69339cae13b0b25835a83d2df390
onjsdnjs <onjsdnjs@gmail.com>
2015-05-07 02:07:02 +0900

    2015.05.07

--- src/main/java/org/corenel/rabbitmqsupport/configurations/RabbitMQConfiguration.java
+++ src/main/java/org/corenel/rabbitmqsupport/configurations/RabbitMQConfiguration.java
@@ -1,9 +1,6 @@
 package org.corenel.rabbitmqsupport.configurations;
 
-import org.corenel.rabbitmqsupport.annotation.RabbitMQConfig;
 import org.corenel.rabbitmqsupport.annotation.RabbitMQContext;
-import org.corenel.rabbitmqsupport.converters.DefaultConverter;
 
 @RabbitMQContext(urls = "1.226.84.208:5672")
-@RabbitMQConfig(converterClass = DefaultConverter.class)
 public interface RabbitMQConfiguration {}
